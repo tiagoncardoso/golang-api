3f43f920f76febb626b2b608457dc6d692f0665e
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2024-09-10 23:19:54 -0300

    feat: add webserver initialization

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,38 @@
 package main
 
-import "github.com/tiagoncardoso/golang-api/configs"
+import (
+	"github.com/tiagoncardoso/golang-api/configs"
+	"github.com/tiagoncardoso/golang-api/internal/entity"
+	"github.com/tiagoncardoso/golang-api/internal/infra/database/sqlite_db"
+	"github.com/tiagoncardoso/golang-api/internal/presenter/controller"
+	"gorm.io/gorm"
+	"log/slog"
+	"net/http"
+)
 
 func main() {
 	config, _ := configs.LoadConfig(".")
-	println(config.DBHost)
+
+	db := initDb(config.DBHost)
+	initWebServer(db)
+}
+
+func initDb(dsn string) *gorm.DB {
+	con := sqlite_db.NewConnect(dsn)
+	db, err := con.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	_ = db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	return db
+}
+
+func initWebServer(db *gorm.DB) {
+	controller.NewProductController(db)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		slog.Error("Error on server start", "msg", err)
+	}
 }
