00bed852027ae8f21f23ba85e8208245753b6be7
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2024-09-12 19:24:43 -0300

    fix: publish initialize routes method

--- internal/presenter/controller/product_controller.go
+++ internal/presenter/controller/product_controller.go
@@ -9,26 +9,28 @@ import (
 
 type ProductUseCases struct {
 	CreateProduct usecase.GeneralInterface
+	Multiplexer   *http.ServeMux
 }
 
-func NewProductController(db *gorm.DB) *ProductUseCases {
+func NewProductController(db *gorm.DB, mux *http.ServeMux) *ProductUseCases {
 	productDB := repository.NewProduct(db)
 	createProductUsecase := usecase.NewProductHandler(productDB)
 
 	return &ProductUseCases{
 		CreateProduct: createProductUsecase,
+		Multiplexer:   mux,
 	}
 }
 
-func (p *ProductUseCases) initializeRoutes() {
-	http.HandleFunc("/product", func(w http.ResponseWriter, r *http.Request) {
+func (p *ProductUseCases) InitializeRoutes() {
+	p.Multiplexer.HandleFunc("/product", func(w http.ResponseWriter, r *http.Request) {
 		err := p.CreateProduct.Execute(r)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
 		}
 
-		w.WriteHeader(http.StatusFound)
+		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte("Product created"))
 	})
 }
